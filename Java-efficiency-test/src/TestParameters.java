0b546abe044377058e37e24dab88ba4e0745f2a1
Rafał Caban <rcaban14@gmail.com>
2025-10-30 16:07:19 +0100

    Add collection type enum returning collector supplier

--- Java-efficiency-test/src/TestParameters.java
+++ Java-efficiency-test/src/TestParameters.java
@@ -1,5 +1,7 @@
-import java.util.Random;
+import java.util.*;
 import java.util.function.Supplier;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class TestParameters {
     public enum DATA_TYPE{
@@ -9,6 +11,23 @@ public class TestParameters {
         private final Supplier<?> dataSupplier;
         DATA_TYPE(Supplier<?> dataSupplier) { this.dataSupplier = dataSupplier; }
         public Supplier<?> getDataSupplier() { return dataSupplier; }
+    }
+
+    public enum COLLECTION_TYPE{
+        ARRAY_LIST(() -> Collectors.toCollection(ArrayList::new)),
+        LINKED_LIST(() -> Collectors.toCollection(LinkedList::new)),
+        HASH_SET(() -> Collectors.toCollection(HashSet::new)),
+        TREE_SET(() -> Collectors.toCollection(TreeSet::new));
+
+        private final Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier;
+
+        COLLECTION_TYPE(Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier) {
+            this.collectorSupplier = collectorSupplier;
+        }
+
+        public Collector<Object, ?, Collection<Object>> getCollector() {
+            return collectorSupplier.get();
+        }
     }
 
     private static TestParameters instance;
