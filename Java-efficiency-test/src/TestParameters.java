b23c5841e169de32ad828e0a95c97f2c9f83a730
Rafał Caban <rcaban14@gmail.com>
2025-10-30 16:05:41 +0100

    Add data type enum returning Supplier

--- Java-efficiency-test/src/TestParameters.java
+++ Java-efficiency-test/src/TestParameters.java
@@ -2,6 +2,15 @@ import java.util.Random;
 import java.util.function.Supplier;
 
 public class TestParameters {
+    public enum DATA_TYPE{
+        INTEGER( () -> new Random().nextInt() ),
+        DOUBLE( () -> new Random().nextDouble() );
+
+        private final Supplier<?> dataSupplier;
+        DATA_TYPE(Supplier<?> dataSupplier) { this.dataSupplier = dataSupplier; }
+        public Supplier<?> getDataSupplier() { return dataSupplier; }
+    }
+
     private static TestParameters instance;
 
     private TestParameters(){}
