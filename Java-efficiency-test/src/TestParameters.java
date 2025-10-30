0c3fca506d38f3b849cf43e2c6b253ec71852058
Rafał Caban <rcaban14@gmail.com>
2025-10-30 16:05:10 +0100

    Implement Singleton pattern for Test Parameters class

--- Java-efficiency-test/src/TestParameters.java
+++ Java-efficiency-test/src/TestParameters.java
@@ -1,1 +1,12 @@
-public class TestParameters {}
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class TestParameters {
+    private static TestParameters instance;
+
+    private TestParameters(){}
+
+    public static TestParameters getInstance(){
+        return TestParameters.instance == null? new TestParameters() : TestParameters.instance;
+    }
+}
