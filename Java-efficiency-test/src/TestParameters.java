ee654589a62f40004b4ef1c44a032e27c94e5e1a
Rafał Caban <rcaban14@gmail.com>
2025-10-30 16:22:03 +0100

    Add collection generator method, getters and setters

--- Java-efficiency-test/src/TestParameters.java
+++ Java-efficiency-test/src/TestParameters.java
@@ -2,18 +2,25 @@ import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TestParameters {
-    public enum DATA_TYPE{
-        INTEGER( () -> new Random().nextInt() ),
-        DOUBLE( () -> new Random().nextDouble() );
+    public enum DATA_TYPE {
+        INTEGER(() -> new Random().nextInt()),
+        DOUBLE(() -> new Random().nextDouble());
 
         private final Supplier<?> dataSupplier;
-        DATA_TYPE(Supplier<?> dataSupplier) { this.dataSupplier = dataSupplier; }
-        public Supplier<?> getDataSupplier() { return dataSupplier; }
+
+        DATA_TYPE(Supplier<?> dataSupplier) {
+            this.dataSupplier = dataSupplier;
+        }
+
+        public Supplier<?> getDataSupplier() {
+            return dataSupplier;
+        }
     }
 
-    public enum COLLECTION_TYPE{
+    public enum COLLECTION_TYPE {
         ARRAY_LIST(() -> Collectors.toCollection(ArrayList::new)),
         LINKED_LIST(() -> Collectors.toCollection(LinkedList::new)),
         HASH_SET(() -> Collectors.toCollection(HashSet::new)),
@@ -31,10 +38,47 @@ public class TestParameters {
     }
 
     private static TestParameters instance;
+    private DATA_TYPE dataType;
+    private COLLECTION_TYPE collectionType;
+    private int numberOfObjects;
 
-    private TestParameters(){}
+    private TestParameters() {
+        this.collectionType = COLLECTION_TYPE.ARRAY_LIST;
+        this.dataType = DATA_TYPE.INTEGER;
+        this.numberOfObjects = 100;
+    }
 
-    public static TestParameters getInstance(){
-        return TestParameters.instance == null? new TestParameters() : TestParameters.instance;
+    public static TestParameters getInstance() {
+        return TestParameters.instance == null ? new TestParameters() : TestParameters.instance;
+    }
+
+    public Collection<?> createCollection(){
+        Supplier<?> dataSupplier = dataType.getDataSupplier();
+        Collector<Object, ?, Collection<Object>> collectorSupplier = collectionType.getCollector();
+
+        return Stream
+                .generate(dataSupplier)
+                .limit(this.numberOfObjects)
+                .collect(collectorSupplier);
+    }
+
+    public void setDataType(DATA_TYPE dataType) {
+        this.dataType = dataType;
+    }
+    public void setCollectionType(COLLECTION_TYPE collectionType) {
+        this.collectionType = collectionType;
+    }
+    public void setNumberOfObjects(int numberOfObjects) {
+        this.numberOfObjects = numberOfObjects;
+    }
+
+    public DATA_TYPE getDataType() {
+        return dataType;
+    }
+    public COLLECTION_TYPE getCollectionType() {
+        return collectionType;
+    }
+    public int getNumberOfObjects() {
+        return numberOfObjects;
     }
 }
