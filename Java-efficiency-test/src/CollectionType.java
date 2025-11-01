d4eb560a81c163589a8893a9a02ec4a8aaed20e1
Rafał Caban <rcaban14@gmail.com>
2025-11-01 19:02:29 +0100

    Add toString() method to enums

--- Java-efficiency-test/src/CollectionType.java
+++ Java-efficiency-test/src/CollectionType.java
@@ -4,18 +4,24 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public enum CollectionType {
-    ARRAY_LIST(() -> Collectors.toCollection(ArrayList::new)),
-    LINKED_LIST(() -> Collectors.toCollection(LinkedList::new)),
-    HASH_SET(() -> Collectors.toCollection(HashSet::new)),
-    TREE_SET(() -> Collectors.toCollection(TreeSet::new));
+    ARRAY_LIST("",() -> Collectors.toCollection(ArrayList::new)),
+    LINKED_LIST("",() -> Collectors.toCollection(LinkedList::new)),
+    HASH_SET("",() -> Collectors.toCollection(HashSet::new)),
+    TREE_SET("",() -> Collectors.toCollection(TreeSet::new));
 
     private final Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier;
+    private final String label;
 
-    CollectionType(Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier) {
+    CollectionType(String label,Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier) {
+        this.label = label;
         this.collectorSupplier = collectorSupplier;
     }
 
     public Collector<Object, ?, Collection<Object>> getCollector() {
         return collectorSupplier.get();
+    }
+    @Override
+    public String toString() {
+        return label;
     }
 }
--- Java-efficiency-test/src/DataType.java
+++ Java-efficiency-test/src/DataType.java
@@ -2,16 +2,23 @@ import java.util.Random;
 import java.util.function.Supplier;
 
 public enum DataType {
-    INTEGER(() -> new Random().nextInt()),
-    DOUBLE(() -> new Random().nextDouble());
+    INTEGER("Integer",() -> new Random().nextInt()),
+    DOUBLE("Double",() -> new Random().nextDouble());
 
     private final Supplier<?> dataSupplier;
+    private final String label;
 
-    DataType(Supplier<?> dataSupplier) {
+    DataType(String label,Supplier<?> dataSupplier) {
+        this.label = label;
         this.dataSupplier = dataSupplier;
     }
 
     public Supplier<?> getDataSupplier() {
         return dataSupplier;
+    }
+
+    @Override
+    public String toString() {
+        return label;
     }
 }
