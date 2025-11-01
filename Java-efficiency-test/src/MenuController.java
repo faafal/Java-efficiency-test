c90d74d11a0673acf48ba23a5b4c6a6f7b46ea84
Rafał Caban <rcaban14@gmail.com>
2025-11-01 18:52:25 +0100

    Add input method

--- Java-efficiency-test/src/MenuController.java
+++ Java-efficiency-test/src/MenuController.java
@@ -1,1 +1,30 @@
-public class MenuController {}
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuController {
+    private static MenuController instance;
+    private final Scanner sc;
+
+    private MenuController(){
+        this.sc = new Scanner(System.in);
+    }
+
+    public static MenuController getInstance() {
+        if (MenuController.instance == null) {
+            MenuController.instance = new MenuController();
+        }
+        return MenuController.instance;
+    }
+
+    public int input(){
+        int input = 0;
+        try {
+            input = sc.nextInt();
+        }catch (InputMismatchException e){
+            System.out.println("It's not a number! Try again.");
+        }
+        return input;
+    }
+
+    public void print(){}
+}
