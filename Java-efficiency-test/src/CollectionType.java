9ed4d09b1ce5c612e636af4fd21225173a9d9c8e
Rafał Caban <rcaban14@gmail.com>
2025-11-02 01:28:08 +0100

    Add starting test and printing results

--- Java-efficiency-test/src/CollectionType.java
+++ Java-efficiency-test/src/CollectionType.java
@@ -4,15 +4,15 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public enum CollectionType {
-    ARRAY_LIST("Array list",() -> Collectors.toCollection(ArrayList::new)),
-    LINKED_LIST("Linked list",() -> Collectors.toCollection(LinkedList::new)),
-    HASH_SET("Hash set",() -> Collectors.toCollection(HashSet::new)),
-    TREE_SET("Tree set",() -> Collectors.toCollection(TreeSet::new));
+    ARRAY_LIST("Array list", () -> Collectors.toCollection(ArrayList::new)),
+    LINKED_LIST("Linked list", () -> Collectors.toCollection(LinkedList::new)),
+    HASH_SET("Hash set", () -> Collectors.toCollection(HashSet::new)),
+    TREE_SET("Tree set", () -> Collectors.toCollection(TreeSet::new));
 
     private final Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier;
     private final String label;
 
-    CollectionType(String label,Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier) {
+    CollectionType(String label, Supplier<Collector<Object, ?, Collection<Object>>> collectorSupplier) {
         this.label = label;
         this.collectorSupplier = collectorSupplier;
     }
--- Java-efficiency-test/src/efficiencyTest.java
+++ Java-efficiency-test/src/efficiencyTest.java
@@ -2,22 +2,27 @@ public class efficiencyTest {
     public static void main(String[] args) {
         MenuController menu = MenuController.getInstance();
         TestParameters test = TestParameters.getInstance();
-        CollectionType[] collectionsTypes = CollectionType.class.getEnumConstants();
+        ResultManager results = ResultManager.getInstance();
+
+        CollectionType[] collectionTypes = CollectionType.class.getEnumConstants();
         DataType[] dataTypes = DataType.class.getEnumConstants();
 
         int userInput = 0;
 
         do {menu.printEnum("Choose collection type", CollectionType.class);
-        } while ( !menu.validateInput(collectionsTypes.length, userInput = menu.input() ) );
-        test.setCollectionType(collectionsTypes[userInput]);
+        } while ( !menu.validateInput(collectionTypes.length, (userInput = menu.input() - 1) ) );
+        System.out.println(userInput);
+        test.setCollectionType(collectionTypes[userInput]);
 
         do {menu.printEnum("Choose data type", DataType.class);
-        } while ( !menu.validateInput(dataTypes.length, userInput = menu.input()) );
+        } while ( !menu.validateInput(dataTypes.length, (userInput = menu.input() - 1) ) );
         test.setDataType(dataTypes[userInput]);
 
         do {System.out.println("Enter number of instances");
-        } while (!menu.validateInput(Integer.MAX_VALUE, userInput = menu.input()));
+        } while (!menu.validateInput(Integer.MAX_VALUE, userInput = menu.input() ));
         test.setNumberOfObjects(userInput);
-        
+
+        test.startTest();
+        results.printResults(test);
     }
 }
