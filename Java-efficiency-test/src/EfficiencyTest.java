fe9d06a314a690118fcdfa6b4cc112c73a85b42f
Rafał Caban <rcaban14@gmail.com>
2025-11-04 12:35:59 +0100

    Add other data types and Fix time units so they dont contain comma which casues problem with csv file format

--- Java-efficiency-test/src/EfficiencyTest.java
+++ Java-efficiency-test/src/EfficiencyTest.java
@@ -2,22 +2,17 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class EfficiencyTest {
     public static void main(String[] args) {
-//        MenuController menu = MenuController.getInstance();
+        MenuController menu = MenuController.getInstance();
         TestParameters test = TestParameters.getInstance();
         ResultManager results = ResultManager.getInstance();
+        System.out.println(Long.MAX_VALUE);
 
+        test.setCollectionType(menu.chooseEnum("Choose collection type", CollectionType.class));
+        test.setDataType(menu.chooseEnum("Choose data type", DataType.class));
+        test.setNumberOfObjects(menu.chooseNumber("Enter number of instances", Long.MAX_VALUE));
 
-//        test.setCollectionType(menu.chooseEnum("Choose collection type", CollectionType.class));
-//        test.setDataType(menu.chooseEnum("Choose data type", DataType.class));
-//        test.setNumberOfObjects(menu.chooseInt("Enter number of instances", Integer.MAX_VALUE));
-
-        for(int i = 0 ; i < 10; i++){
-            test.setDataType(DataType.values()[ThreadLocalRandom.current().nextInt(DataType.values().length)]);
-            test.setCollectionType(CollectionType.values()[ThreadLocalRandom.current().nextInt(CollectionType.values().length)]);
-            test.setNumberOfObjects(ThreadLocalRandom.current().nextInt(1_000_000, 10_000_000));
-            test.startTest();
-            results.saveResults(test);
-        }
+        test.startTest();
+        results.saveResults(test);
         results.printAll();
     }
 }
